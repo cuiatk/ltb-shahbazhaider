9af446055ad8e99dc7fb1805e034a62121439c41
shahbazhaider <39879330+shahbazhaider@users.noreply.github.com>
2019-06-13 11:02:02 +0500

    Update Customer.java

--- src/labTerminalB/Customer.java
+++ src/labTerminalB/Customer.java
@@ -1,76 +1,50 @@
 package labTerminalB;
 
-import java.util.Enumeration;
-import java.util.Vector;
+public class Driver {
+	/*
+	 * TODO 1	10 Marks
+	 * Write a regular express at line 10 so that you can remove all html tags from a given string
+	 * after you write regular expression the out put should be
+	 * The Good, the Bad, and the Ugly
+	 * after completing each TODO task commit and push to remote repository with commit message
+	 * 		"TODO 'TASK NUMBER' Complete" e.g. in this case "TODO 1 COMPLETED"
+	 */
 
-public class Customer {
-		private String _name;
-		private Vector<Rental> _rentals = new Vector<Rental>();
-		public Customer (String name){
-			_name = name;
-		};
-		public void addRental(Rental arg) {
-			_rentals.addElement(arg);
-		}
-		public String getName (){
-			return _name;
-		};
+	public static void main(String[] args) 
+{
+    
+    
+}
+	//Q2	
+        String input = "The <b>Good</b>, the <i>Bad</i>, and the <strong>Ugly</strong>";
+             
+                
+           String noHTMLString = input.replaceAll("\\<.*?>","");
 		
-		/*
-		 * TODO 4		20 Marks
-		 * Identify the bad smells in the statement method and refactor the code
-		 * In case you add a new method also provide specifications for new method
-		 * after refactoring all test cases must still be passed
-		 * Commit after each change and include a meaningful message about the change you made
-		 * e.g. Method Move methodName();
-		 * 
-		 */
+	System.out.println(noHTMLString);
+
+
+	public static void main(String[] args) {
+		String input = "The <b>Good</b>, the <i>Bad</i>, and the <strong>Ugly</strong>";
+		String regex = "TODO 1";
+		String output = input.replaceAll(regex, "");
+		System.out.println(output);
+
 		
-		/**
-		 * TODO 3		10 Marks
-		 * Provide suitable specification for statement method 
-		 */
-		public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
-		Enumeration<Rental> rentals = _rentals.elements();
-		String result = "Rental Record for " + getName() + "\n";
-		while (rentals.hasMoreElements()) {
-			double thisAmount = 0;
-			Rental each = (Rental) rentals.nextElement();
-			//determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
-			}
-			// add frequent renter points
-			frequentRenterPoints ++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&&
-					each.getDaysRented() > 1) frequentRenterPoints ++;
-			//show figures for this rental
-			result += "\t" + each.getMovie().getTitle()+ "\t" +
-					String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
-		}
-		//add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) +
-				"\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
-		+
-		" frequent renter points";
-		return result;
+		
+		Movie m1 = new Movie("Spancer", 0);
+		Movie m2 = new Movie("Speed", 1);
+		Movie m3 = new Movie("Blood Diamond",2);
+		Customer me = new Customer("Nadir");
+		Customer me2 = new Customer("Allah Ditta");
+		Rental r1 = new Rental(m1, 23);
+		Rental r2 = new Rental(m2,4);
+		Rental r3 = new Rental(m3,3);
+		me.addRental(r1);
+		me.addRental(r2);
+		me2.addRental(r3);
+		System.out.println(me.statement());
+		System.out.println(me2.statement());
+
 	}
 }
